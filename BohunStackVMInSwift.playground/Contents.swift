8a96cbbb0e0c661220c3c0994c10bd8708c7f8b5
Brian Hill <brian@lingerhere.org>
2022-07-19 18:03:47 -0700

    Finished first video.

--- BohunStackVMInSwift.playground/Contents.swift
+++ BohunStackVMInSwift.playground/Contents.swift
@@ -1,13 +1,15 @@
 /*:
-
-# Bohun Stack VM in Swift
  
-_A Swift playground based on [this YouTube playlist](https://youtube.com/playlist?list=PLSiFUSQSRYAOFwfP-aMzXJlWKVyIuWfPU) by Philip Bohun_
-
-Bohun's implementation is in C++. As we follow along in the videos, we translate his code into Swift.
+ # Bohun Stack VM in Swift
  
-Alternatively, we could begin with [this website](https://www.jmeiners.com/lc3-vm/) by Justin Meiners. It is in C.
-
+ _A Swift playground based on [this YouTube playlist](https://youtube.com/playlist?list=PLSiFUSQSRYAOFwfP-aMzXJlWKVyIuWfPU) by Philip Bohun_
+ 
+ Bohun's implementation is in C++. As we follow along in the videos, we translate his code into Swift.
+ 
+ At present the implementation our implementation is only of the first video in his playlist.
+ 
+ Alternatively, we could begin with [this website](https://www.jmeiners.com/lc3-vm/) by Justin Meiners. It is in C.
+ 
  */
 
 /*
@@ -21,10 +23,6 @@ Alternatively, we could begin with [this website](https://www.jmeiners.com/lc3-v
  * 3 => undefined
  */
 
-/* At present the implementation is complete up to the 13-minute point of the first video.
- * The next thing Bohun will do is implement getData().
- */
-
 typealias i32 = UInt32
 
 class StackVM {
@@ -33,33 +31,75 @@ class StackVM {
     var memory = [i32](repeating: 0, count: 1000000)
     var typ: i32 = 0
     var dat: i32 = 0
-
+    var running: i32 = 1
+    
     func getType(instruction: i32) -> i32 {
         let typeMask: i32 = 0xc0000000
         return (typeMask & instruction) >> 30
     }
     
     func getData(instruction: i32) -> i32 {
-        return 0
+        let dataMask: i32 = 0x3fffffff
+        return dataMask & instruction
     }
     
     func fetch() {
+        pc += 1
     }
     
     func decode() {
-    }
-    
-    func execute() {
+        let instruction = memory[Int(pc)]
+        typ = getType(instruction: instruction)
+        dat = getData(instruction: instruction)
     }
     
     func doPrimitive() {
+        switch(dat) {
+        case 0: // halt
+            print("halt")
+            running = 0
+        case 1: // add
+            let operandy = memory[Int(sp) - 1]
+            let operandx = memory[Int(sp)]
+            print("add \(operandy) \(operandx)")
+            memory[Int(sp) - 1] = UInt32(operandx + operandy)
+            sp -= 1
+        default: // noop
+            break
+        }
+    }
+    
+    func execute() {
+        if (typ == 0 || typ == 2) {
+            sp += 1
+            memory[Int(sp)] = dat
+        } else {
+            doPrimitive()
+        }
     }
     
     func run() {
+        pc -= 1
+        while running == 1 {
+            fetch()
+            decode()
+            execute()
+            let tos = memory[Int(sp)]
+            print("tos: \(tos)")
+        }
     }
     
     func loadProgram(prog: [i32]) {
-        
+        var i: Int = 0
+        while i < prog.count {
+            memory[Int(pc) + i] = prog[i]
+            i += 1
+        }
     }
     
 }
+
+let vm = StackVM()
+let prog: [i32] = [UInt32(3), UInt32(4), UInt32(0x40000001), UInt32(0x40000000)]
+vm.loadProgram(prog: prog)
+vm.run()
